751c4b1754dacf2a9a4d8f0e64e2351f1618043f
triducal <thebingobangoyt@gmail.com>
2024-10-24 20:53:11 -0500

    refactor: moved app mounting to controllers

--- src/Client/Controllers/GuiController.lua
+++ src/Client/Controllers/GuiController.lua
@@ -1,7 +1,14 @@
+local Players = game:GetService("Players")
+local App = require("@Client/App")
+local Log = require("@Shared/Log")
+local Vide = require("@Packages/Vide")
+
 local GuiController = {}
 
 function GuiController:Init()
-	--Pass
+	local PlayerGui = Players.LocalPlayer:WaitForChild("PlayerGui")
+	Vide.mount(App, PlayerGui)
+	Log.info("Mounted App")
 end
 
 return GuiController
--- src/Client/runtime.client.lua
+++ src/Client/runtime.client.lua
@@ -1,21 +1,13 @@
-local Players = game:GetService("Players")
 local CharmSync = require("@Packages/CharmSync")
 local Loader = require("@Packages/Loader")
-local Vide = require("@Packages/Vide")
 
-local App = require("@Client/App")
 local Log = require("@Shared/Log")
 local Remotes = require("@Shared/Remotes")
 local atoms = require("@Shared/Store/atoms")
 
-local function LoadServices()
+local function LoadControllers()
 	local Services = script.Parent.Controllers
 	Loader.SpawnAll(Loader.LoadDescendants(Services, Loader.MatchesName("Controller$")), "Init")
-end
-
-local function MountApp()
-	local PlayerGui = Players.LocalPlayer:WaitForChild("PlayerGui")
-	Vide.mount(App, PlayerGui)
 end
 
 local function SyncCharm()
@@ -29,10 +21,9 @@ local function SyncCharm()
 end
 
 local function Start()
-	MountApp()
 	SyncCharm()
-	LoadServices()
-	Log.info("Server initialized")
+	LoadControllers()
+	Log.info("Client initialized")
 end
 
 Start()
